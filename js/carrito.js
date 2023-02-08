bb0023f22b8747cb8bb2bceb43f979152857d118
Victor Ramirez <victor_remodelado@hotmail.com>
2023-02-07 21:05:13 -0600

    Site completed and running properly

---
+++ js/carrito.js
@@ -0,0 +1,27 @@
+//  const email = document.querySelector("#email");
+//  const iconl = document.querySelector(".icon1");
+//  const icon2 = document.querySelector(".icon2");
+//  
+//  let regExp = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+//  function check (){
+//      if(email.value.match(regExp)){
+//          email.style.borderColor="#27ae60";
+//          email.style.background="#eafaf1";
+//          icon1.style.display="none";
+//          icon2.style.display="block";
+//      }else{
+//          email.style.borderColor="#e74c3c";
+//          email.style.background="#fceae9";
+//          icon1.style.display="block";
+//          icon2.style.display="none";
+//      }
+//  
+//      if(email.value==""){
+//  
+//          email.style.borderColor="lightgrey";
+//          email.style.background="#fff";
+//          icon1.style.display="none";
+//          icon2.style.display="none";
+//      }
+//  }
+
